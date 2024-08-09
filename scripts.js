2bbfab711b267183e0fe78e00fbebd9bab60ac1d
joaopedrofonteles <dev.fonteles@gmail.com>
2024-08-09 11:56:54 -0300

    feat: add script for dynamic UI updates

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let prevButton = document.getElementById('prev')
+let nextButton = document.getElementById('next')
+let container = document.querySelector('.container')
+let items = container.querySelectorAll('.list .item')
+let indicator = document.querySelector('.indicators')
+let dots = indicator.querySelectorAll('ul li')
+
+
+nextButton.onclick = () => {
+  console.log('botão next')
+  
+}
+
+prevButton.onclick = () => {
+  console.log('botão prev')
+}
+
